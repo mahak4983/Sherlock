2e8d9c58e099268b3d7d8b01e6b129ab628d6f3a
mahak4983 <mahak4983@gmail.com>
2023-04-15 19:50:17 +0530

    files added

---
+++ L1/L1.js
@@ -0,0 +1,2 @@
+setTimeout(function () { alert(" Here is a Hint for you! Try converting from a different base"); }, 60000*15);
+setTimeout(function () { alert(" Here another Hint for you! Octal to text"); }, 60000*20);
---
+++ L2/L2.js
@@ -0,0 +1,2 @@
+setTimeout(function () { alert(" Heres a clue for you! Try going through every html element"); }, 60000*15);
+
